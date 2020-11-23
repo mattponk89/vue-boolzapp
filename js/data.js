77ac0bcc931e52c7c627a88c03240def27c8040f
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-11-23 10:28:26 +0000

    Milestone X

--- js/data.js
+++ js/data.js
@@ -1,46 +1,159 @@
-const dbDomande = [{
-  question: "1",
-  answers: [
-    "2",
-    "5",
-    "10",
-    "15",
-    "20"],
-  correctAnswer: "10"
-},{
-  question: "2",
-  answers: [
-    "2",
-    "5",
-    "10",
-    "15",
-    "20"],
-  correctAnswer: "10"
-},{
-  question: "3",
-  answers: [
-    "2",
-    "5",
-    "10",
-    "15",
-    "20"],
-  correctAnswer: "10"
-},{
-  question: "4",
-  answers: [
-    "2",
-    "5",
-    "10",
-    "15",
-    "20"],
-  correctAnswer: "10"
-},{
-  question: "5",
-  answers: [
-    "2",
-    "5",
-    "10",
-    "15",
-    "20"],
-  correctAnswer: "10"
-}]
+//dati dei contatti boolzApp
+
+var dati = [
+  {
+    id:0,
+    nome : 'Flavio Teacher',
+    avatarContact : 'img/avatar_4.jpg',
+    lastSeen: "10:30",
+    risposteSolite: [
+      "Questo argomento è un pò tricky!",
+      "Sono sicuro che riuscirai a trovare la soluzione.",
+      "Non fare il lavativo!"
+    ],
+    chat:[
+      {
+        text: "Flavio posso farti una domanda?",
+        mittente: 1
+      },
+      {
+        text: "Certo! Dimmi tutto..",
+        mittente: 0
+      }
+    ]
+  },
+  {
+    id:1,
+    nome : 'Alessandro Sensei',
+    avatarContact : 'img/avatar_5.jpg',
+    lastSeen: "18:45",
+    risposteSolite: [
+      "Hai pushato? Io so già la risposta a questa domanda..",
+      "Se vedo una table per il layout potrei fare una strage",
+      "Questo pomeriggio facciamo un ripasso, ci vediamo su Zoom!"
+    ],
+    chat:[
+      {
+        text: "Ciao, non riesco a capire come mai non mi funziona.. l'esercizio per me è perfetto!",
+        mittente: 1
+      },
+      {
+        text: "Controlla bene a riga 16, hai fatto un errore semplice. Ti suona?",
+        mittente: 0
+      }
+    ]
+  },
+  {
+    id:2,
+    nome : 'Francesco Teacher',
+    avatarContact : 'img/avatar_3.jpg',
+    lastSeen: "13:35",
+    risposteSolite: [
+      "Non preoccuparti! Ora troviamo la soluzione assieme..",
+      "Collegati su Zoom, così ti spiego a voce!",
+      "Sono sicuro che raggiungerai la soluzione.. se hai bisogno scrivimi pure."
+    ],
+    chat:[
+      {
+        text: "Ciao, mi puoi aiutare? Non riesco a capire come implementare questa funzione del programma..",
+        mittente: 1
+      },
+      {
+        text: "Certo, dimmi il tuo problema..",
+        mittente: 0
+      }
+    ]
+  },
+  {
+    id:3,
+    nome : 'Marco Tab',
+    avatarContact : 'img/avatar_2.jpg',
+    lastSeen: "11:11",
+    risposteSolite: [
+      "Mi piace il crossfit",
+      "Mi piace programmare..",
+      "Matt è un lavativo.."
+    ],
+    chat:[
+      {
+        text: "Ciao Marco! Come stai?",
+        mittente: 1
+      },
+      {
+        text: "Ciao Matt, tutto bene tu?",
+        mittente: 0
+      }
+    ]
+  },
+  {
+    id:4,
+    nome : 'Mirko',
+    avatarContact : 'img/avatar_io.jpg',
+    lastSeen: "9:33",
+    risposteSolite: [
+      "Sono proprio bello!",
+      "Hai ascoltato la mia nuova canzone?",
+      "Perché Luisa ha la barba?"
+    ],
+    chat:[
+      {
+        text: "Bella Mirko! Sei un grande..",
+        mittente: 1
+      },
+      {
+        text: "Ciaooo.. Grazie grazie lo so!",
+        mittente: 0
+      }
+    ]
+  },
+  {
+    id:5,
+    nome : 'Giulia',
+    avatarContact : 'img/avatar_6.jpg',
+    lastSeen: "12:12",
+    risposteSolite: [
+      "Aiutiamoci a vicenda!",
+      "Sono euforica!",
+      "Potere alle donne."
+    ],
+    chat:[
+      {
+        text: "Heila Matt, tutto bene?",
+        mittente: 1
+      },
+      {
+        text: "Ciao! Tutto ok tu?",
+        mittente: 0
+      },
+      {
+        text: "Certo!",
+        mittente: 1
+      }
+    ]
+  },
+  {
+    id:6,
+    nome : 'Mirelle',
+    avatarContact : 'img/avatar_9.jpg',
+    lastSeen: "7:18",
+    risposteSolite: [
+      "Z",
+      "Y",
+      "X"
+    ],
+    chat:[
+      {
+        text: "A",
+        mittente: 1
+      },
+      {
+        text: "B",
+        mittente: 0
+      },
+      {
+        text: "C",
+        mittente: 1
+      }
+    ]
+  }
+]
--- js/main.js
+++ js/main.js
@@ -1,28 +1,49 @@
-// Vue.config.devtools = true
-// const appQuiz = new Vue({
-//   el: '#root',
-//   data: {
-//     contatoreScheda:0,
-//     rispostaAttuale: 0,
-//     risultatoUtente: 0,
-//     nonfinito: true,
-//     scheda: [{
-//       rispostaGiusta: "10",
-//       domanda: "2*5",
-//       risposte: ["2","5","10","15","20"],
-//       rispostaUtente: 0
-//       }
-//     ]
-//   },
-//   methods: {
-//     next(){
-//       if(this.contatoreScheda < this.scheda.length){
-//         this.scheda[this.contatoreScheda].rispostaUtente = this.rispostaAttuale;
-//         this.contatoreScheda++;
-//       }
-//       if(this.contatoreScheda == (this.scheda.length-1)){
-//         this.nonfinito = false;
-//       }
-//     }
-//   }
-// });
+Vue.config.devtools = true
+
+const boolzApp = new Vue({
+  el: '#appWindow',
+  data: {
+    chatSelector: 0, //selettore della chat
+    inputSearchContact: "", //ricerca contatto
+    message: { //oggetto messaggio con testo e mittente
+      text: "",
+      mittente: 1
+    },
+    userAccount: {
+      userAvatar: "img/avatar_io.jpg",
+      userName: "Matteo Ponchietti"
+    }, // variabile nel file data.js
+    contacts : [...dati],
+  },
+  methods: { //funzione cambia chat al click del contatto
+    changeChat: function(index){
+      this.chatSelector = index
+    }, //funzione invio messaggio in chat con risposta automatica
+    sendMessage: function(index){
+      let newMessage = {...this.message}
+      this.contacts[index].chat.push(newMessage);
+      this.clearInputMessage();
+      setTimeout(this.contactReply, 3000, index)
+    }, // azzero il textbox del messaggio dopo averlo inviato.
+    clearInputMessage: function(){
+      this.message.text = "";
+    }, // funzione per la risposta automatica con selezione random tra più risposte possibili
+    contactReply: function(index){
+      const randomNum = Math.floor(Math.random() * (this.contacts[index].risposteSolite.length))
+      const messageReply = {
+        text: this.contacts[index].risposteSolite[randomNum],
+        mittente: 0
+      }
+      this.contacts[index].chat.push(messageReply);
+    }
+
+  },
+  computed:{ //creo un array dei contatti filtrati con la ricerca
+    contactsFiltered: function(){
+      return this.contacts.filter(el =>{
+        return el.nome.toLowerCase().includes(this.inputSearchContact.toLowerCase())
+      })
+    }
+
+  }
+});
